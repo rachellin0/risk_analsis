a98d8d1933f0e7929f71d07638137d0bc96bfa0a
rachellin0 <91289121+rachellin0@users.noreply.github.com>
2024-05-15 00:42:04 -0400

    Update Black-Scholes.py

--- Black-Scholes.py
+++ Black-Scholes.py
@@ -1,4 +1,7 @@
-
+#price: Perform risk analysis calculations for a portfolio of option contracts and fixed-income securities 
+#○ For option contracts, calculate the theoretical price using the Black-Scholes model. 
+#○ For fixed-income securities, calculate the duration and convexity. 
+#○ If time permits, calculate Value at Risk (VaR) for the options and bonds portfolios.
 
 import pandas as pd
 import numpy as np
